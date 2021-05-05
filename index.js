171758ef3b595a7160fec4af3a92cf0a6242940e
Teofil-Ioan Vartolomei <teo.vty@gmail.com>
2021-05-05 08:39:19 +0200

    Add functionality

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('Hello');
+const chalk = require('chalk');
+const randomColor = require('randomcolor');
+
+const color = randomColor();
+const str = `
+#############################
+#############################
+#############################
+#####                   #####
+#####     ${color}       #####
+#####                   #####
+#############################
+#############################
+#############################`;
+
+if (process.argv[3] && process.argv[3] === 'dark') {
+  console.log(chalk.keyword(process.argv[2]).dim(str));
+} else if (process.argv[3] && process.argv[3] === 'light') {
+  console.log(chalk.keyword(process.argv[2]).bold(str));
+} else if (!process.argv[2]) {
+  console.log(chalk.hex(color)(str));
+} else {
+  console.log(chalk.keyword(process.argv[2])(str));
+}
